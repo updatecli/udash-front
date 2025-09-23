fd5a4c6494067867b341e632d3ba22ce112c96fd
Olivier Vernin <olivier@vernin.me>
2025-09-23 09:55:02 +0200

    chore: refactor target component

--- src/components/pipeline/_target.vue
+++ src/components/pipeline/_target.vue
@@ -1,160 +1,308 @@
 <template>
-   <v-toolbar
-    density="compact"
-    class="text-white"
-   >
-    <v-toolbar-title>
-      <v-icon :icon="getStatusIcon(data.Result)" :color="getStatusColor(data.Result)"></v-icon>  {{ data.Name }}
-    </v-toolbar-title>
+    <v-toolbar
+        density="compact"
+        class="text-white"
+    >
+        <v-toolbar-title>
+            <v-icon
+                :icon="getStatusIcon(data.Result)"
+                :color="getStatusColor(data.Result)"
+            ></v-icon>  {{ data.Name }}
+        </v-toolbar-title>
 
-    <v-toolbar-items>
-      <v-btn 
-        v-if="data.DryRun == true"
-      >
-        DryRun
-      </v-btn>
-      <v-btn
-      >
-        {{ id }}
-      </v-btn>
-            
-    </v-toolbar-items>
-   </v-toolbar>
+        <v-toolbar-items>
+            <v-btn v-if="data.DryRun == true">
+                DryRun
+            </v-btn>
+            <v-btn>{{ id }}</v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
 
-  <v-container fluid>
-    <v-card variant="flat">
-
-      <v-card-subtitle v-if="data.Scm.URL">
-        <v-row>
-          <v-col
-            class="text-left"
-          >
-            <v-icon icon="mdi-git"></v-icon>
-          </v-col>
-          <v-col
-            class="text-center"
-          >
-            {{ data.Scm.URL }}
-          </v-col>
-          <v-col
-            class="text-right"
-          >
-            {{ data.Scm.Branch.Source }}
-          </v-col>
+    <v-container fluid>
+        <v-row class="mb-4">
+            <v-col cols="12">
+                <v-card flat class="pa-3">
+                    <v-table class="metadata-table">
+                        <thead>
+                            <tr class="bg-grey-lighten-4">
+                                <th class="font-weight-bold" style="width: 25%">ID</th>
+                                <th class="font-weight-bold" style="width: 25%">Status</th>
+                                <th class="font-weight-bold" style="width: 25%">Kind</th>
+                                <th class="font-weight-bold" style="width: 25%">Mode</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="font-weight-medium">{{ id }}</td>
+                                <td>
+                                    <v-chip
+                                        :color="getStatusColor(data.Result)"
+                                        size="small"
+                                        variant="flat"
+                                        class="text-white"
+                                    >
+                                        <v-icon size="x-small" class="mr-1">
+                                            {{ getStatusIcon(data.Result) }}
+                                        </v-icon>
+                                        {{ getStatusText(data.Result) }}
+                                    </v-chip>
+                                </td>
+                                <td>
+                                    <v-chip variant="outlined" size="small">
+                                        {{ data.Config?.Kind || 'N/A' }}
+                                    </v-chip>
+                                </td>
+                                <td>
+                                    <v-chip
+                                        :color="data.DryRun ? 'warning' : 'success'"
+                                        variant="outlined"
+                                        size="small"
+                                    >
+                                        {{ data.DryRun ? 'Dry Run' : 'Live' }}
+                                    </v-chip>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </v-table>
+                </v-card>
+            </v-col>
         </v-row>
-        <v-divider></v-divider>
-      </v-card-subtitle>
 
-      <v-card-text>
-          {{ data.Description }}
-      </v-card-text>
+        <v-row>
+            <v-col cols="12" lg="8" md="8" sm="12">
+                <v-card v-if="data.Scm.URL" variant="flat" class="mb-4">
+                    <v-card-title class="d-flex align-items-center pb-2">
+                        <v-icon class="mr-2">mdi-git</v-icon>
+                        Source Control
+                    </v-card-title>
+                    <v-card-text class="pt-0">
+                        <v-row>
+                            <v-col cols="12" md="8">
+                                <div class="d-flex align-items-center">
+                                    <v-icon size="small" class="mr-2" color="grey-darken-1">mdi-link</v-icon>
+                                    <span class="text-body-2">{{ data.Scm.URL }}</span>
+                                </div>
+                            </v-col>
+                            <v-col cols="12" md="4">
+                                <div class="d-flex align-items-center">
+                                    <v-icon size="small" class="mr-2" color="grey-darken-1">mdi-source-branch</v-icon>
+                                    <span class="text-body-2">{{ data.Scm.Branch.Source }}</span>
+                                </div>
+                            </v-col>
+                        </v-row>
+                    </v-card-text>
+                </v-card>
 
-      <v-card-actions
-        v-if="isInformationUpdated()"
-      >
-          <v-spacer></v-spacer>
-          <v-btn
-            v-if="data.Information"
-            variant="outlined"
-          >
-            <p>{{ sanitizedInformation }}</p>
-          </v-btn>
+                <v-card variant="flat">
+                    <v-card-title class="d-flex align-items-center">
+                        <v-icon class="mr-2">mdi-text</v-icon>
+                        Description
+                    </v-card-title>
+                    <v-card-text>
+                        <p class="text-body-1">{{ data.Description || 'No description available' }}</p>
+                    </v-card-text>
 
-          <v-btn
-            icon="mdi-arrow-right"
-            v-if="data.NewInformation"
-          ></v-btn>
+                    <v-card-actions v-if="data.Information || data.NewInformation">
+                        <v-expansion-panels variant="accordion" elevation="0" class="w-100">
+                            <v-expansion-panel v-if="isInformationUpdated()">
+                                <v-expansion-panel-title>
+                                    <v-icon class="mr-2" size="small">mdi-compare-horizontal</v-icon>
+                                    Information Changes
+                                </v-expansion-panel-title>
+                                <v-expansion-panel-text>
+                                    <v-row>
+                                        <v-col cols="12" md="5">
+                                            <div class="text-subtitle-2 mb-2">
+                                                <v-icon size="small" class="mr-1">mdi-minus</v-icon>
+                                                Previous
+                                            </div>
+                                            <div class="information-content bg-red-lighten-5">
+                                                {{ data.Information }}
+                                            </div>
+                                        </v-col>
+                                        <v-col cols="12" md="2" class="text-center d-flex align-center justify-center">
+                                            <v-icon>mdi-arrow-right</v-icon>
+                                        </v-col>
+                                        <v-col cols="12" md="5">
+                                            <div class="text-subtitle-2 mb-2">
+                                                <v-icon size="small" class="mr-1">mdi-plus</v-icon>
+                                                New
+                                            </div>
+                                            <div class="information-content bg-green-lighten-5">
+                                                {{ data.NewInformation }}
+                                            </div>
+                                        </v-col>
+                                    </v-row>
+                                </v-expansion-panel-text>
+                            </v-expansion-panel>
 
-          <v-btn
-            variant="outlined"
-            v-if="data.NewInformation"
-          >
-            {{ sanitizedNewInformation }}
-          </v-btn>
-          <v-spacer></v-spacer>
-      </v-card-actions>
-      <v-card-actions
-        v-else
-      >
-          <v-spacer></v-spacer>
-          <v-btn
-            v-if="data.NewInformation"
-            variant="outlined"
-          >
-            <p>{{ sanitizedNewInformation }}</p>
-          </v-btn>
-          <v-spacer></v-spacer>
-      </v-card-actions>
-    </v-card>
-  </v-container>
+                            <v-expansion-panel v-else-if="data.NewInformation">
+                                <v-expansion-panel-title>
+                                    <v-icon class="mr-2" size="small">mdi-information-outline</v-icon>
+                                    Information
+                                </v-expansion-panel-title>
+                                <v-expansion-panel-text>
+                                    <div class="information-content">
+                                        {{ data.NewInformation }}
+                                    </div>
+                                </v-expansion-panel-text>
+                            </v-expansion-panel>
+                        </v-expansion-panels>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
 
-  <ConsoleOutputComponent
-    v-if="data.ConsoleOutput"
-    :data="data.ConsoleOutput"
-  ></ConsoleOutputComponent>
+            <v-col cols="12" lg="4" md="4" sm="12">
+                <v-card flat class="config-card">
+                    <v-card-title class="d-flex align-items-center">
+                        <v-icon class="mr-2">mdi-cog</v-icon>
+                        Configuration
+                    </v-card-title>
 
-  <ChangelogComponent
-    v-if="data.Changelogs"
-    :data="data.Changelogs"
-  ></ChangelogComponent>
+                    <v-card-text>
+                        <div v-if="data.Config?.Spec" class="mb-4">
+                            <div class="d-flex align-items-center mb-2">
+                                <h5 class="text-subtitle-2 font-weight-medium">Specification</h5>
+                            </div>
+                            <v-card variant="outlined" class="pa-2">
+                                <div class="yaml-container">
+                                    <pre><code v-highlight class="language-yaml">{{ toYAML(data.Config.Spec) }}</code></pre>
+                                </div>
+                            </v-card>
+                        </div>
 
+                        <div v-if="data.Config?.Transformers" class="mb-4">
+                            <div class="d-flex align-items-center mb-2">
+                                <h5 class="text-subtitle-2 font-weight-medium">Transformers</h5>
+                            </div>
+                            <v-card variant="outlined" class="pa-2">
+                                <div class="yaml-container">
+                                    <pre><code v-highlight class="language-yaml">{{ toYAML(data.Config.Transformers) }}</code></pre>
+                                </div>
+                            </v-card>
+                        </div>
+
+                        <div v-if="!data.Config?.Spec && !data.Config?.Transformers" class="text-center pa-4">
+                            <v-icon size="large">mdi-file-outline</v-icon>
+                            <p class="text-grey-darken-1 mt-2">No configuration available</p>
+                        </div>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+
+    <ConsoleOutputComponent
+        v-if="data.ConsoleOutput"
+        :data="data.ConsoleOutput"
+    ></ConsoleOutputComponent>
+
+    <ChangelogComponent
+        v-if="data.Changelogs"
+        :data="data.Changelogs"
+    ></ChangelogComponent>
 </template>
 
 <script>
+import ConsoleOutputComponent from './_consoleOutput.vue'
+import ChangelogComponent from './_changelog.vue'
 
-  import ConsoleOutputComponent from './_consoleOutput.vue'
-  import ChangelogComponent from './_changelog.vue'
+import { getStatusColor, getStatusIcon, getStatusText } from '@/composables/status';
+import { toYAML } from '@/composables/yaml'
 
-  import { getStatusColor, getStatusIcon } from '@/composables/status';
+export default {
+    name: "TargetComponent",
 
-    export default {
-        name: "TargetComponent",
+    components: {
+        ConsoleOutputComponent,
+        ChangelogComponent,
+    },
 
-        components: {
-          ConsoleOutputComponent,
-          ChangelogComponent,
-        },
-
-        computed: {
-          sanitizedInformation: function(){
-              if (this.data.Information.length > 72) {
+    computed: {
+        sanitizedInformation: function(){
+            // Updated limit to 72 characters like in _source.vue
+            if (this.data.Information && this.data.Information.length > 72) {
                 return this.data.Information.substring(0, 69) + "..."
-              }
-
-              return this.data.Information
-          },
-          sanitizedNewInformation: function(){
-              if (this.data.NewInformation.length > 72) {
-                return this.data.NewInformation.substring(0, 69) + "..."
-              }
-
-              return this.data.NewInformation
-          },
-        },
-
-        data: () => ({
-        }),
-
-        props: [
-            'data',
-            'id'
-        ],
-
-        methods: {
-            isInformationUpdated: function(){
-                if (this.data.Information == "unknown") {
-                  return false
-                }
-
-                return this.data.Information !== this.data.NewInformation
-            },
-            getStatusColor: function(input){
-              return getStatusColor(input);
-            },
-            getStatusIcon: function(input){
-              return getStatusIcon(input);
             }
-        }
+            return this.data.Information || ''
+        },
+        sanitizedNewInformation: function(){
+            // Updated limit to 72 characters like in _source.vue
+            if (this.data.NewInformation && this.data.NewInformation.length > 72) {
+                return this.data.NewInformation.substring(0, 69) + "..."
+            }
+            return this.data.NewInformation || ''
+        },
+    },
+
+    data: () => ({
+    }),
+
+    props: [
+        'data',
+        'id'
+    ],
+
+    methods: {
+        toYAML,
+        getStatusText,
+        getStatusColor,
+        getStatusIcon,
+        isInformationUpdated: function(){
+            if (this.data.Information == "unknown") {
+                return false
+            }
+            return this.data.Information !== this.data.NewInformation
+        },
     }
+}
 </script>
+
+<style scoped>
+
+.metadata-table {
+    font-size: 1rem;
+}
+
+.metadata-table th {
+    font-size: 1.1rem;
+    font-weight: 600;
+    padding: 12px 16px;
+    background-color: rgba(0, 0, 0, 0.02);
+}
+
+.metadata-table td {
+    font-size: 1rem;
+    padding: 12px 16px;
+    font-weight: 500;
+}
+
+.yaml-container {
+    max-height: 300px;
+    overflow-y: auto;
+    background-color: #f5f5f5;
+    border-radius: 4px;
+}
+
+.yaml-container pre {
+    margin: 0;
+    font-size: 0.85rem;
+    line-height: 1.4;
+}
+
+.config-card {
+    position: sticky;
+    top: 20px;
+}
+
+.information-content {
+    white-space: pre-wrap;
+    word-break: break-word;
+    font-family: 'Roboto Mono', monospace;
+    background-color: #f5f5f5;
+    padding: 12px;
+    border-radius: 4px;
+    font-size: 0.9rem;
+}
+</style>
 
