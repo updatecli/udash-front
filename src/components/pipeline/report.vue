b82eac737ad2f91b58a649f0c9eb825ceeb5699c
Olivier Vernin <olivier@vernin.me>
2025-12-18 22:03:18 +0100

    fix: regression
    Signed-off-by: Olivier Vernin <olivier@vernin.me>

--- src/components/pipeline/report.vue
+++ src/components/pipeline/report.vue
@@ -333,7 +333,7 @@ export default {
     },
 
     getPipelineReportLink: function(id){
-      return `${getApiBaseURL()}/pipeline/reports/` + id
+      return "/pipeline/reports/" + id
     },
 
     cancelAutoUpdate() {
