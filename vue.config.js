71c62cdec210d9c10dd889b8f09426e71d0f0e39
Olivier Vernin <olivier@vernin.me>
2026-04-05 09:55:44 +0200

    chore: add eslint configuration
    Signed-off-by: Olivier Vernin <olivier@vernin.me>

--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,39 @@
 const { defineConfig } = require('@vue/cli-service')
+const EslintWebpackPlugin = require('eslint-webpack-plugin')
+
 module.exports = defineConfig({
   transpileDependencies: true,
 
   devServer: {
-      proxy: {
-        "^/api": {
-          target: 'http://localhost:8080',
-          ws: true,
-          changeOrigin: true,
-          pathRewrite: {
-            '^/api' : '/api' 
-            },
-          },
-      },  
-  },  
+    proxy: {
+      "^/api": {
+        target: 'http://localhost:8080',
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/api'
+        },
+      },
+    },
+  },
 
+  chainWebpack: config => {
+    config.plugins.delete('eslint')
+    config.plugin('eslint').use(EslintWebpackPlugin, [{
+      extensions: undefined,
+      context: __dirname,
+      cwd: __dirname,
+      configType: 'flat',
+      failOnError: true,
+      formatter: 'stylish',
+    }])
+  },
   //  make this app "location agnostic"
   publicPath: './',
-
   pluginOptions: {
     vuetify: {
-            // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-        }
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    }
   }
 })
 
