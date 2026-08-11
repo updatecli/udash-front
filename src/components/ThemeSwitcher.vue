12cf1c3f2e8cf3e210cc68bdaa930748c45479e8
Olivier Vernin <olivier@vernin.me>
2026-08-11 19:34:17 +0200

    fix: explicitely track theme switch
    Signed-off-by: Olivier Vernin <olivier@vernin.me>

--- src/components/ThemeSwitcher.vue
+++ src/components/ThemeSwitcher.vue
@@ -1,38 +1,20 @@
 <template>
   <v-menu>
     <template #activator="{ props }">
-      <v-btn
-        class="mt-5 mr-5"
-        icon="mdi-theme-light-dark"
-        v-bind="props"
-        variant="flat"
-        density="comfortable"
-      />
+      <v-btn icon="mdi-theme-light-dark" v-bind="props" variant="flat" density="compact" />
     </template>
 
     <v-list>
-      <v-list-item
-        title="Light"
-        prepend-icon="mdi-weather-sunny"
-        @click="setTheme('light')"
-      />
-      <v-list-item
-        title="Dark"
-        prepend-icon="mdi-weather-night"
-        @click="setTheme('dark')"
-      />
-      <v-list-item
-        title="System"
-        prepend-icon="mdi-monitor"
-        @click="setTheme('system')"
-      />
+      <v-list-item title="Light" prepend-icon="mdi-weather-sunny" @click="setTheme('light')" />
+      <v-list-item title="Dark" prepend-icon="mdi-weather-night" @click="setTheme('dark')" />
+      <v-list-item title="System" prepend-icon="mdi-monitor" @click="setTheme('system')" />
     </v-list>
   </v-menu>
 </template>
 
 <script setup>
 
-import { ref, watch } from 'vue'
+import { onBeforeUnmount, ref, watch } from 'vue'
 import { useTheme } from 'vuetify'
 import { getStorageKey } from '@/composables/runtime';
 
@@ -65,6 +47,23 @@ watch(mode, value => {
   localStorage.setItem(THEME_STORAGE_KEY, value)
 }, {
   immediate: true,
+})
+
+// In 'system' mode the OS preference is only read when applyTheme runs, so without
+// this listener the theme would freeze at whatever the OS was set to when the
+// component mounted. It used to be re-read by accident, because every navigation
+// remounted the whole app shell; now that the shell lives in App.vue and mounts
+// once, tracking the change has to be explicit.
+function onSystemThemeChange() {
+  if (mode.value === 'system') {
+    applyTheme('system')
+  }
+}
+
+systemDark.addEventListener('change', onSystemThemeChange)
+
+onBeforeUnmount(() => {
+  systemDark.removeEventListener('change', onSystemThemeChange)
 })
 
 </script>
