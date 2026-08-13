f08f05c40082e8a09d6be13e44671ca10c775c48
Olivier Vernin <me@olblak.com>
2026-08-13 13:36:47 +0200

    fix: search by date label (#271)
    Signed-off-by: Olivier Vernin <olivier@vernin.me>

--- src/composables/date.js
+++ src/composables/date.js
@@ -63,18 +63,6 @@ export function formatToLayout(date) {
       return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}${tzOffset}`
 }
 
-export function formatToLayoutWithoutTimezone(date) {
-      const year = date.getFullYear()
-      const month = String(date.getMonth() + 1).padStart(2, '0')
-      const day = String(date.getDate()).padStart(2, '0')
-      const hours = String(date.getHours()).padStart(2, '0')
-      const minutes = String(date.getMinutes()).padStart(2, '0')
-      const seconds = String(date.getSeconds()).padStart(2, '0')
-
-      return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
-}
-
-
 export function getStartTimeFromStorage() {
     try{
         const storedFilter = localStorage.getItem(FILTER_STORAGE_KEY)
